61205c2e80ee99c98aabce3548f92b2da3594c5d
Ethan.OToole <eotoole005@gmail.com>
2025-04-09 13:53:42 -0400

    completed ShoppingList App

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        String[] items = {"Beef", "Chicken", "Chips", "Water", "Milk", "Eggs (if affordable", "Sauce", "Drinks", "Rice", "Cheese"};
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+    }
+}
