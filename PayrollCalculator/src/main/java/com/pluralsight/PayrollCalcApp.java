614a25741b883078f03f0f730ec11ac408ac5f10
Ethan.OToole <eotoole005@gmail.com>
2025-04-10 14:20:17 -0400

    Updated Payroll Calculator with overtime pay

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalcApp.java
@@ -7,8 +7,8 @@ public class PayrollCalcApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String name;
-        float hoursWorked, payRate, result;
-
+        float hoursWorked, payRate, overtimeHours, overtimePayTotal;
+        float result = 0;
         System.out.print("Enter your name please: ");
         name = input.nextLine();
 
@@ -18,9 +18,20 @@ public class PayrollCalcApp {
         System.out.print("Enter your pay rate: ");
         payRate = input.nextFloat();
 
-        result = hoursWorked * payRate;
+        if(hoursWorked > 40)
+        {
+            //Calculates overtime pay first than calculates normal hours worked after.
+            overtimeHours = hoursWorked - 40;
+            overtimePayTotal = overtimeHours * (payRate * 1.5f);
+            result = (hoursWorked - overtimeHours) * payRate + overtimePayTotal;
+        }
+        else
+        {
+            //No overtime will just have a result.
+            result = hoursWorked * payRate;
+        }
 
-        System.out.printf("%s " + "your gross pay is: " + "%.2f", name, result);
-        
+        System.out.printf("%s " + "your gross pay is: " + "$%.2f", name, result);
+
     }
 }
