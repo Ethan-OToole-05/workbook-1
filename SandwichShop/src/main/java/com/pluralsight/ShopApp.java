920ff7e69022cda4cb9c2c9335c4b56170aef735
Ethan.OToole <eotoole005@gmail.com>
2025-04-11 11:59:17 -0400

    Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/ShopApp.java
+++ SandwichShop/src/main/java/com/pluralsight/ShopApp.java
@@ -10,17 +10,28 @@ public class ShopApp {
         float largePrice = 8.95f;
         float total = 0;
         float totalDiscount = 0;
-        char option;
+        char option, loadedOption;
         int age;
 
         System.out.println("Welcome to the sandwich shop! ");
         System.out.print("Would you like a (R)egular sandwich or a (L)arge sandwich? ");
         option = input.next().charAt(0);
         option = Character.toUpperCase(option);
+        System.out.print("Would you like the sandwich loaded? (Y/N): ");
+        loadedOption = input.next().charAt(0);
+        loadedOption = Character.toUpperCase(loadedOption);
         if(option == 'R'){
-            total = 5.45f;
+            if(loadedOption == 'Y'){
+                total = 5.45f + 1.00f;
+            } else {
+                total = 5.45f;
+            }
         } else if (option == 'L') {
-            total = 8.95f;
+            if(loadedOption == 'Y') {
+                total = 8.95f + 1.75f;
+            } else {
+                total = 8.95f;
+            }
         }
         else {
             System.out.println("Invalid input");
